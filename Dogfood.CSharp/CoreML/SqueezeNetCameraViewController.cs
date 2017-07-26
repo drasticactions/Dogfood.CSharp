f085cc19e5bea6943bfb70b2fa18f445342f0121
Tim Miller <innerlogic4321@gmail.com>
2017-07-26 14:53:53 -0400

    Add SqueezeNetCameraViewController and models

---
+++ Dogfood.CSharp/CoreML/SqueezeNetCameraViewController.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using UIKit;
+using Foundation;
+using CoreGraphics;
+using AVFoundation;
+using CoreFoundation;
+using CoreMedia;
+using CoreVideo;
+using CoreML;
+using XibFree;
+
+namespace Dogfood.CSharp.CoreML
+{
+	public class SqueezeNetCameraViewController : UIViewController, IAVCaptureVideoDataOutputSampleBufferDelegate
+	{
+		public SqueezeNetCameraViewController()
+		{
+			LoadModel();
+		}
+		static AVCaptureSession session;
+		MLModel model;
+
+		Lazy<AVCaptureVideoPreviewLayer> previewLayer = new Lazy<AVCaptureVideoPreviewLayer>(() =>
+		{
+			if (session == null)
+				return null;
+
+			var previewLayer = new AVCaptureVideoPreviewLayer(session)
+			{
+				VideoGravity = AVLayerVideoGravity.ResizeAspectFill
+			};
+			return previewLayer;
+		});
+
+		UILabel uiMessage;
+		AVCaptureDevice frontCamera = AVCaptureDevice.GetDefaultDevice(AVCaptureDeviceType.BuiltInWideAngleCamera, AVMediaType.Video, AVCaptureDevicePosition.Back);
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			PrepareSession();
+			session.StartRunning();
+		}
+
+		public override void ViewDidLayoutSubviews()
+		{
+			base.ViewDidLayoutSubviews();
+			previewLayer.Value.Frame = View.Frame;
+		}
+
+		public override void ViewDidAppear(bool animated)
+		{
+
+			base.ViewDidAppear(animated);
+
+			if (!previewLayer.IsValueCreated)
+				return;
+
+			RotateVideo();
+
+			var layout = new LinearLayout(Orientation.Vertical)
+			{
+				SubViews = new View[]
+				{
+					new LinearLayout(Orientation.Vertical) {
+						LayoutParameters = new LayoutParameters()
+									{
+										Width = AutoSize.FillParent,
+										Height = AutoSize.FillParent,
+									},
+						SubViews = new View[] {
+							new LinearLayout(Orientation.Vertical) {
+								Layer = previewLayer.Value,
+								LayoutParameters = new LayoutParameters()
+									{
+										Width = AutoSize.FillParent,
+										Height = AutoSize.FillParent,
+									}
+							},
+							new NativeView() {
+								View = uiMessage = new UILabel() {
+									Text = "",
+									Font = UIFont.SystemFontOfSize(25),
+									BackgroundColor = UIColor.Black,
+									TextColor = UIColor.White,
+									Lines = 0
+								},
+								LayoutParameters = new LayoutParameters()
+								{
+									Width = AutoSize.FillParent,
+									Height = AutoSize.WrapContent,
+									Gravity = Gravity.CenterHorizontal
+								}
+							}
+						}
+					}
+				},
+			};
+			this.View = new XibFree.UILayoutHost(layout);
+		}
+
+		public override void ViewWillTransitionToSize(CGSize toSize, IUIViewControllerTransitionCoordinator coordinator)
+		{
+			base.ViewWillTransitionToSize(toSize, coordinator);
+
+			if (!previewLayer.IsValueCreated) return;
+
+			RotateVideo();
+		}
+
+		void RotateVideo()
+		{
+			var videoPreviewLayerConnection = previewLayer.Value.Connection;
+			if (videoPreviewLayerConnection != null)
+			{
+				var deviceOrientation = UIDevice.CurrentDevice.Orientation;
+
+				AVCaptureVideoOrientation newVideoOrientation;
+				if (!TryConvertToVideoOrientation(deviceOrientation, out newVideoOrientation))
+					return;
+				if (!deviceOrientation.IsPortrait() && !deviceOrientation.IsLandscape())
+					return;
+
+				videoPreviewLayerConnection.VideoOrientation = newVideoOrientation;
+			}
+		}
+
+		static bool TryConvertToVideoOrientation(UIDeviceOrientation orientation, out AVCaptureVideoOrientation result)
+		{
+			switch (orientation)
+			{
+				case UIDeviceOrientation.Portrait:
+					result = AVCaptureVideoOrientation.Portrait;
+					return true;
+
+				case UIDeviceOrientation.PortraitUpsideDown:
+					result = AVCaptureVideoOrientation.PortraitUpsideDown;
+					return true;
+
+				case UIDeviceOrientation.LandscapeLeft:
+					result = AVCaptureVideoOrientation.LandscapeRight;
+					return true;
+
+				case UIDeviceOrientation.LandscapeRight:
+					result = AVCaptureVideoOrientation.LandscapeLeft;
+					return true;
+
+				default:
+					result = 0;
+					return false;
+			}
+		}
+
+		void PrepareSession()
+		{
+			session = new AVCaptureSession();
+			var captureDevice = frontCamera;
+
+			if (session == null || captureDevice == null)
+				return;
+
+			try
+			{
+				var deviceInput = new AVCaptureDeviceInput(captureDevice, out var deviceInputError);
+				if (deviceInputError != null)
+					throw new NSErrorException(deviceInputError);
+
+				session.BeginConfiguration();
+
+				if (session.CanAddInput(deviceInput))
+					session.AddInput(deviceInput);
+
+				var output = new AVCaptureVideoDataOutput
+				{
+					WeakVideoSettings = new CVPixelBufferAttributes { PixelFormatType = CVPixelFormatType.CV32BGRA }.Dictionary,
+					AlwaysDiscardsLateVideoFrames = true
+				};
+
+				if (session.CanAddOutput(output))
+					session.AddOutput(output);
+
+				session.CommitConfiguration();
+
+				var queue = new DispatchQueue("output.queue");
+				output.SetSampleBufferDelegateQueue(this, queue);
+
+				Console.WriteLine($"PrepareSession: Done setting up delegate");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"PrepareSession Error: {ex.Message}");
+			}
+		}
+
+		[Export("captureOutput:didOutputSampleBuffer:fromConnection:")]
+		public void DidOutputSampleBuffer(AVCaptureOutput captureOutput, CMSampleBuffer sampleBuffer, AVCaptureConnection connection)
+		{
+			using (var image = ImageUtils.CreateImage(sampleBuffer))
+			{
+				MakePrediction(image);
+			}
+			sampleBuffer.Dispose();
+		}
+
+		IMLFeatureProvider CreateInput(UIImage image)
+		{
+			var pixelBuffer = image.Resize(new CGSize(227, 227)).ToPixelBuffer();
+
+			var imageValue = MLFeatureValue.FromPixelBuffer(pixelBuffer);
+
+			var inputs = new NSDictionary<NSString, NSObject>(new NSString("image"), imageValue);
+
+			return new MLDictionaryFeatureProvider(inputs, out var error);
+		}
+
+		void MakePrediction(UIImage image)
+		{
+			IMLFeatureProvider input = CreateInput(image);
+
+			var output = model.GetPrediction(input, out var error);
+
+			if (error != null)
+			{
+				Console.WriteLine($"Error predicting: {error}");
+				return;
+			}
+
+			var classLabel = output.GetFeatureValue("classLabel").StringValue;
+
+			var message = $"{classLabel.ToUpperInvariant()}: {output.GetFeatureValue("classLabelProbs").DictionaryValue[classLabel]}";
+
+			Console.WriteLine(message);
+			BeginInvokeOnMainThread(() =>
+			{
+				if (uiMessage != null)
+					uiMessage.Text = message;
+			});
+		}
+
+		void LoadModel()
+		{
+			var modelUrl = NSBundle.MainBundle.GetUrlForResource("SqueezeNet", "mlmodelc");
+
+			model = MLModel.FromUrl(modelUrl, out var error);
+
+			if (error != null)
+			{
+				Console.WriteLine($"Error writing model: {error}");
+			}
+			else
+			{
+				Console.WriteLine($"Loaded model: {model}");
+			}
+		}
+	}
+}
+
---
+++ Dogfood.CSharp/Utilities/ImageUtils.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using CoreGraphics;
+using CoreImage;
+using CoreMedia;
+using CoreVideo;
+using UIKit;
+
+namespace Dogfood.CSharp
+{
+	public static class ImageUtils
+	{
+		public static UIImage CreateImage(CMSampleBuffer sampleBuffer)
+		{
+			using (var pixelBuffer = sampleBuffer.GetImageBuffer() as CVPixelBuffer)
+			{
+				// Lock the base address
+				pixelBuffer.Lock(CVPixelBufferLock.None);
+				// Get the number of bytes per row for the pixel buffer
+				var baseAddress = pixelBuffer.BaseAddress;
+				int bytesPerRow = (int)pixelBuffer.BytesPerRow;
+				int width = (int)pixelBuffer.Width;
+				int height = (int)pixelBuffer.Height;
+				var flags = CGBitmapFlags.PremultipliedFirst | CGBitmapFlags.ByteOrder32Little;
+				// Create a CGImage on the RGB colorspace from the configured parameter above
+				using (var cs = CGColorSpace.CreateDeviceRGB())
+				using (var context = new CGBitmapContext(baseAddress, width, height, 8, bytesPerRow, cs, (CGImageAlphaInfo)flags))
+				using (var cgImage = context.ToImage())
+				{
+					pixelBuffer.Unlock(CVPixelBufferLock.None);
+					return UIImage.FromImage(cgImage);
+				}
+			}
+		}
+
+		public static UIImage CreateImage(CIImage ciImage)
+		{
+			var context = CIContext.FromOptions(null);
+			var cgimage = context.CreateCGImage(ciImage, ciImage.Extent);
+			return UIImage.FromImage(cgimage);
+		}
+
+		public static UIImage Resize(this UIImage self, CGSize newSize)
+		{
+			UIGraphics.BeginImageContextWithOptions(new CGSize(width: newSize.Width, height: newSize.Height), true, 1.0f);
+			self.Draw(new CGRect(x: 0, y: 0, width: newSize.Width, height: newSize.Height));
+			var resizedImage = UIGraphics.GetImageFromCurrentImageContext();
+			UIGraphics.EndImageContext();
+			return resizedImage;
+		}
+
+		public static CVPixelBuffer ToPixelBuffer(this UIImage self)
+		{
+			var width = self.Size.Width;
+
+			var height = self.Size.Height;
+
+			var attrs = new CVPixelBufferAttributes();
+			attrs.CGBitmapContextCompatibility = true;
+			attrs.CGImageCompatibility = true;
+
+			var resultPixelBuffer = new CVPixelBuffer((int)(width),
+					(int)(height),
+					CVPixelFormatType.CV32ARGB,
+											 attrs);
+
+			resultPixelBuffer.Lock(CVPixelBufferLock.None);
+			var pixelData = resultPixelBuffer.GetBaseAddress(0);
+
+			var rgbColorSpace = CGColorSpace.CreateDeviceRGB();
+
+			var context = new CGBitmapContext(data: pixelData,
+										  width: (int)(width),
+										  height: (int)(height),
+										  bitsPerComponent: 8,
+									bytesPerRow: resultPixelBuffer.GetBytesPerRowOfPlane(0),
+										  colorSpace: rgbColorSpace,
+											   bitmapInfo: CGImageAlphaInfo.NoneSkipFirst);
+
+			context.TranslateCTM(tx: 0, ty: height);
+			context.ScaleCTM(sx: 1.0f, sy: -1.0f);
+
+			UIGraphics.PushContext(context);
+
+			self.Draw(new CGRect(x: 0, y: 0, width: width, height: height));
+
+			UIGraphics.PopContext();
+
+			resultPixelBuffer.Unlock(CVPixelBufferLock.None);
+
+			return resultPixelBuffer;
+		}
+	}
+}
