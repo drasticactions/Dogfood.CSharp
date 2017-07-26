1a61416a27c125de569ff2cf517a88492ae1ad9d
Tim Miller <innerlogic4321@gmail.com>
2017-07-26 14:45:22 -0400

    Add modified MainMenuViewController, fix Info.plist

--- Dogfood.CSharp/MainMenuViewController.cs
+++ Dogfood.CSharp/MainMenuViewController.cs
@@ -9,5 +9,55 @@ namespace Dogfood.CSharp
         public MainMenuViewController (IntPtr handle) : base (handle)
         {
         }
+
+		public override void ViewDidLoad()
+		{
+			string[] tableItems = new string[]
+			{
+				"CoreML.SqueezeNetCameraViewController"
+			};
+			this.TableView.Source = new TableSource(tableItems, this);
+			this.TableView.ReloadData();
+		}
     }
+
+	public class TableSource : UITableViewSource
+	{
+
+		string[] TableItems;
+		string CellIdentifier = "TableCell";
+		UIViewController Owner;
+
+		public TableSource(string[] items, UIViewController owner)
+		{
+			Owner = owner;
+			TableItems = items;
+		}
+
+		public override nint RowsInSection(UITableView tableview, nint section)
+		{
+			return TableItems.Length;
+		}
+
+		public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
+		{
+			var controllerName = TableItems[indexPath.Row];
+			var controller = Activator.CreateInstance(Type.GetType($"Dogfood.CSharp.{controllerName}")) as UIViewController;
+			Owner.NavigationController.PushViewController(controller, true);
+		}
+
+		public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+		{
+			UITableViewCell cell = tableView.DequeueReusableCell(CellIdentifier);
+			string item = TableItems[indexPath.Row];
+
+			//---- if there are no cells to reuse, create a new one
+			if (cell == null)
+			{ cell = new UITableViewCell(UITableViewCellStyle.Default, CellIdentifier); }
+
+			cell.TextLabel.Text = item;
+
+			return cell;
+		}
+	}
 }
