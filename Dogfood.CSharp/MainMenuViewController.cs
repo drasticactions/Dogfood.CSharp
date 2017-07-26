b7700835355266b11464db4d199c1e3955580d14
Tim Miller <innerlogic4321@gmail.com>
2017-07-26 14:42:23 -0400

    Add MainMenuViewController

---
+++ Dogfood.CSharp/MainMenuViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace Dogfood.CSharp
+{
+    public partial class MainMenuViewController : UITableViewController
+    {
+        public MainMenuViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
